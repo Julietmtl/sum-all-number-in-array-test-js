3ef01a67cbd7c452b60668742d468724c3e2ee05
Juliet <li_juliet@hotmail.com>
2021-10-30 21:21:23 -0400

    complete recursion function

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let total = 0;
+  // Sum all the numbers in the array by recursion
+  for (let item of array) {
+    if (Array.isArray(item)) {
+      total += sumItems(item);
+    } else {
+      total += item;
+    }
+  }
+  return total;
 }
 
 module.exports = sumItems;
